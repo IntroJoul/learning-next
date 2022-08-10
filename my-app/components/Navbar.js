bf92e2bc431e787f7e273b5cba994c10e6bd1bd5
IntroJoul <introjouldev@gmail.com>
2022-08-10 23:54:24 +0100

    temporary

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -13,24 +13,13 @@ export default function Navbar() {
         <BoostrapNavbar.Toggle aria-controls="responsive-BoostrapNavbar-nav" />
         <BoostrapNavbar.Collapse id="responsive-BoostrapNavbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#features">placeholder</Nav.Link>
+            <Nav.Link href="#pricing">placeholder</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link href="#deets">More placeholders</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+              another placeholder lol
             </Nav.Link>
           </Nav>
         </BoostrapNavbar.Collapse>
